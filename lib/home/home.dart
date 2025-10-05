211034e66e3624bfd9a178d581a60095e93f373f
Kendi J <kendyjaky@gmail.com>
2025-10-05 02:00:19 +0200

    Added some texts

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -11,20 +11,55 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Center(
           child: Column(
             children: [
               Image.asset("assets/doodles/iG1.png"),
               Text(Texts.intro),
+              SizedBox(height: 150),
+              Text(
+                Texts.about,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal,
+                  fontSize: 80
+                ),
+              ),
               Image.asset("assets/doodles/iG2.png"),
-              Text("data"),
+              Container(
+                padding: EdgeInsets.fromLTRB(200, 0, 200, 0),
+                child: Text(Texts.me),
+              ),
+              SizedBox(height: 150),
+              Text(
+                Texts.still,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal,
+                  fontSize: 80
+                ),
+              ),
               Image.asset("assets/doodles/iG3.png"),
-              Text("data"),
+              Text(Texts.interest),
+              SizedBox(height: 150),
+              Text(
+                Texts.rest,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal,
+                  fontSize: 80
+                ),
+              ),
               Image.asset("assets/doodles/iG4.png"),
-              Text("data"),
-
+              Text(Texts.cows),
+              SizedBox(height: 150),
+              Row(
+                children: [
+                  IconButton(onPressed: () {}, icon: Icon(Icons.abc_outlined))
+                ],
+              )
             ],
           ),
         ),
